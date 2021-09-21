3e71da4b4d6ab5161ae5e580759ae4ff7f9fe1bf
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-21 19:55:05 -0300

    Add C-Sharp Type Examples

---
+++ 02 Speaking C#/C-Sharp Type Examples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace C_Sharp_Type_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            double valorDecimalDouble = 10.35783;
+            float valorDecimalFloat = 2.43f;
+            int num = 32;
+            long numLong = 32L;
+            string nome = "Maria";
+            char caracter = 'F';
+
+            int num2 = default(int);
+
+            Console.WriteLine(num2);
+            Console.WriteLine("O valor é " + valorDecimalDouble.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine("O valor é " + valorDecimalDouble.ToString("F1"));
+
+            Console.WriteLine("\r\n " + valorDecimalDouble + " " + num + "  " + nome);
+
+            Console.WriteLine("\r\nInT max " + int.MaxValue);
+            Console.WriteLine("InT min" + int.MinValue);
+        }
+    }
+}
