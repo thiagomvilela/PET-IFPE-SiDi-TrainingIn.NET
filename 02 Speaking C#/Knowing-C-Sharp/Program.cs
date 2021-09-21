667df2bb51da27a4451f8b0a41ea3e07f48d23fc
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-21 19:56:05 -0300

    Add Knowing-C-Sharp

---
+++ 02 Speaking C#/Knowing-C-Sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Knowing_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Thiago!");
+            Console.WriteLine("Temperature on {0:D} is {1} ºC", DateTime.Today, 23.4);
+        }
+    }
+}
