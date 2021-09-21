03d1c17741efb8c41f321ded0e30e14a550efda9
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-21 19:51:05 -0300

    Replace World by Thiago

--- 01 Hello, C#! Welcome, .NET!/First-App/Program.cs
+++ 01 Hello, C#! Welcome, .NET!/First-App/Program.cs
@@ -6,7 +6,7 @@ namespace First_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Thiago!");
         }
     }
 }
