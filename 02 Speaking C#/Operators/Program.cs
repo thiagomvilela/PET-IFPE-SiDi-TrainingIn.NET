71742683256f36d40962fbac88a945cf0d80cece
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-21 19:57:50 -0300

    Add Operators

---
+++ 02 Speaking C#/Operators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = a++;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("\r\n");
+
+            int c = 10;
+            int d = ++c;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine("\r\n");
+
+            int e = 10;
+            int f = e--;
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine("\r\n");
+
+            int g = 10;
+            int h = --g;
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+        }
+    }
+}
