a4d89bf5c2193c8537a2c663386a4426af350c2d
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-21 20:05:58 -0300

    Add Var and Dynamic

---
+++ 03 Controlling Flow and Converting Types/Var and Dynamic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Var_and_Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            VarExample.Example();
+        }
+    }
+}
---
+++ 03 Controlling Flow and Converting Types/Var and Dynamic/VarExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Var_and_Dynamic
+{
+    class VarExample
+    {
+        internal static void Example()
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+            dynamic a = 10;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a.Thiago);
+        }
+    }
+}
