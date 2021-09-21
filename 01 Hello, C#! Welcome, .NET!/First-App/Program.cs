ee90ab10c393a312bd83e660a8a20735efee7a7a
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-21 19:50:20 -0300

    Hello World

---
+++ 01 Hello, C#! Welcome, .NET!/First-App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
