fd415450cef7637de5eae5773fd6d5d1844ea126
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-22 08:41:17 -0300

    Add Switch Example

---
+++ 03 Controlling Flow and Converting Types/Switch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchExample.Example01();
+            SwitchExample.Example02();
+            SwitchExample.SwitchExpressionExample();
+
+        }
+    }
+}
