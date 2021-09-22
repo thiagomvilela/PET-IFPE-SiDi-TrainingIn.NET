6a3e09238c72ddcbd829b77d3e7fabed2e224861
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-22 09:28:28 -0300

    Add While - Do While

---
+++ 03 Controlling Flow and Converting Types/While - Do While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While___Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        
+
+        
+            string password = string.Empty;
+            do
+            {
+                Console.Write("Enter your password: ");
+                password = Console.ReadLine();
+            }
+            while (password != "Pa$$word");
+            Console.WriteLine("Correct!");
+        }
+    }
+}
