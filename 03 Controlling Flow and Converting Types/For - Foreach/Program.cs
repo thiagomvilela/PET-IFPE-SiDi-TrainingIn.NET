dfa81e3ceab8da0667712210aa953d3cafd0cb1f
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-09-22 09:38:37 -0300

    Add For - Foreach

---
+++ 03 Controlling Flow and Converting Types/For - Foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For___Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.WriteLine(y);
+            }
+
+            string[] names = {"Adam", "Barry", "Charlie"};
+            
+            foreach (string name in names)
+            {
+                Console.WriteLine($"\n{name} has {name.Length} characters.");
+            }
+        }
+    }
+}
